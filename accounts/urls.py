2ee0adfcdf32e81bc5e839c7d9a54917e43bcbb1
Fredrik Wrede <fredrik@scaleoutsystems.com>
2022-03-22 15:28:36 +0100

    add auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import SignUpView, auth
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('auth/', auth)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.http import HttpResponse
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def auth(request):
+    if request.user.is_authenticated:
+        return HttpResponse(status=200)
+    return HttpResponse(status=401)
